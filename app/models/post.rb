9226f0c385d85dd044f40c6e176ed8578155e20c
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-05-16 20:05:18 -0400

    commit tree 5f0b5ba7a28481a7202a766a55e475ca885bcf9a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+  validate :title_does_not_include?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i,
+  ]
+
+  def title_does_not_include?
+    if CLICKBAIT_PATTERNS.none? { |pattern| pattern.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
